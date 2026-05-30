3f05933550c96c5428a0099a4d5e47e0c30095d2
Oleksii-Moloiko <oleksiimoloiko@gmail.com>
2026-05-30 22:14:27 +0300

    update

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn office_booking.wsgi
+web: python manage.py collectstatic --noinput && gunicorn office_booking.wsgi
--- office_booking/settings.py
+++ office_booking/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -135,7 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
